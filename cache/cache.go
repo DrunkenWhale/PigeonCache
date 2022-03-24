88e58f6c3b3cecd79821fbe497211213d40ac4d0
pigeon_gugugu <892197240@qq.com>
2022-03-24 17:11:42 +0800

    feat: delete method

--- cache/cache.go
+++ cache/cache.go
@@ -7,6 +7,7 @@ type PigeonCache struct {
 	usedMemory int64
 	list       *list.List
 	cache      map[string]*list.Element // *list.Element is a point to a node in PigeonCache.list
+	feedback   func(key string, value Value)
 }
 
 type entity struct {
@@ -18,12 +19,13 @@ type Value interface {
 	Len() int
 }
 
-func New(maxMemory int64) *PigeonCache {
+func New(maxMemory int64, feedback func(string, Value)) *PigeonCache {
 	return &PigeonCache{
 		maxMemory:  maxMemory,
 		usedMemory: 0,
 		list:       list.New(),
 		cache:      make(map[string]*list.Element),
+		feedback:   feedback,
 	}
 }
 
@@ -37,4 +39,16 @@ func (pigeon *PigeonCache) Get(key string) (value Value, ok bool) {
 	}
 }
 
+func (pigeon *PigeonCache) RemoveOldest() {
+	element := pigeon.list.Back()
+	if element != nil {
+		pigeon.list.Remove(element)
+		kv := element.Value.(*entity)
+		delete(pigeon.cache, kv.key)
+		pigeon.usedMemory -= int64(kv.value.Len()) + int64(len(kv.key))
+		if pigeon.feedback != nil {
+			pigeon.feedback(kv.key, kv.value)
+		}
+	}
+}
 
