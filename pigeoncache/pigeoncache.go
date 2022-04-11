9d0a1dc26738f14cc9cdde08e278ca91ce4667ab
pigeon_gugugu <892197240@qq.com>
2022-04-11 21:06:41 +0800

    feat: Group struct

---
+++ pigeoncache/pigeoncache.go
@@ -0,0 +1,48 @@
+package pigeoncache
+
+import "sync"
+
+type Getter interface {
+	Get(key string) ([]byte, error)
+}
+
+type GetterFunc func(key string) ([]byte, error)
+
+func (f GetterFunc) Get(key string) ([]byte, error) {
+	return f(key)
+}
+
+type Group struct {
+	name      string
+	getter    Getter
+	mainCache cache
+}
+
+var (
+	mutex  sync.RWMutex
+	groups = make(map[string]*Group)
+)
+
+func NewGroup(name string, cacheBytes int64, getter Getter) *Group {
+	if getter == nil {
+		panic("nil Getter")
+	}
+	mutex.Lock()
+	defer mutex.Unlock()
+	g := &Group{
+		name:   name,
+		getter: getter,
+		mainCache: cache{
+			cacheBytes: cacheBytes,
+		},
+	}
+	groups[name] = g
+	return g
+}
+
+func GetGroup(name string) *Group {
+	mutex.RLock()
+	defer mutex.RUnlock()
+	g := groups[name]
+	return g
+}
