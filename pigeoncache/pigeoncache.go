50eefc5457a41b22b297be5235b3f34ce76f1148
pigeon_gugugu <892197240@qq.com>
2022-04-11 21:29:14 +0800

    feat: Group method

--- pigeoncache/pigeoncache.go
+++ pigeoncache/pigeoncache.go
@@ -1,6 +1,10 @@
 package pigeoncache
 
-import "sync"
+import (
+	"fmt"
+	"log"
+	"sync"
+)
 
 type Getter interface {
 	Get(key string) ([]byte, error)
@@ -45,4 +49,33 @@ func GetGroup(name string) *Group {
 	defer mutex.RUnlock()
 	g := groups[name]
 	return g
+}
+
+func (g *Group) Get(key string) (ByteView, error) {
+	if key == "" {
+		return ByteView{}, fmt.Errorf("empty key")
+	}
+	if v, ok := g.mainCache.get(key); ok {
+		log.Println("hit")
+		return v, nil
+	}
+	return g.load(key)
+}
+
+func (g *Group) load(key string) (value ByteView, err error) {
+	return g.getLocally(key)
+}
+
+func (g *Group) getLocally(key string) (ByteView, error) {
+	bytes, err := g.getter.Get(key)
+	if err == nil {
+		return ByteView{}, err
+	}
+	value := ByteView{b: cloneBytes(bytes)}
+	g.populateCache(key, value)
+	return value, nil
+}
+
+func (g *Group) populateCache(key string, value ByteView) {
+	g.mainCache.add(key, value)
 }
